7c5df2a793f74d19b50aed5be472144a60b10d8b
ellie-ctrl35 <enyatepe.ac@st.vvu.edu.gh>
2024-06-23 15:15:44 +0000

    chore: Update fonts and styles for better UI consistency

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
@@ -6,23 +6,18 @@ import Notifications from './pages/Notification/Notifications';
 import Settings from './pages/Settings';
 
 function App() {
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
-
   return (
-    <div style={{width:"100vw",height:"100vh"}}>
-
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/notification" element={<Notifications />} />
-      <Route path="/settings" element={<Settings />} />
-    </Routes>
-    
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/notification" element={<Notifications />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
